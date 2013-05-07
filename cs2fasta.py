29c9860cfbc3c8807a344a5a31e443fa655be033
Michael Ding <yandy.ding@gmail.com>
2013-05-07 19:44:20 +0800

    add "filter" to translator

--- cs2fasta.py
+++ cs2fasta.py
@@ -8,13 +8,23 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Translate csfasta file to fasta file")
     parser.add_argument("inputs", type = str, metavar='N', nargs='+', help = "The csfasta files")
+    parser.add_argument("-f", "--filte", action='store_true', default=False, help = "The csfasta files")
+    parser.add_argument("-t", "--cutoff", type= int, default = 12, help = "if option 'filte' is True, set the cutoff for quality")
+    parser.add_argument("-m", "--min", type= int, default = 25, help = "if option 'filte' is True, set the min length for quality")
     args = parser.parse_args(sys.argv[1:])
     kwargs = vars(args)
     try:
         for idx in range(len(kwargs['inputs'])):
             (pre, ext) = os.path.splitext(kwargs['inputs'][idx])
-            output = "%s.fasta" % pre
-            t = Translate(kwargs['inputs'][idx], output)
+            if kwargs['filte']:
+                output = "%s.filted.fasta" % pre
+                qual = "%s.qual" % pre
+                cutoff = kwargs['cutoff']
+                min_len = kwargs['min']
+                t = Translate(kwargs['inputs'][idx], output, qual, cutoff, min_len)
+            else:
+                output = "%s.fasta" % pre
+                t = Translate(kwargs['inputs'][idx], output)
             t.translate()
     except SolidError, e:
         print e
--- pysolidqa/translate.py
+++ pysolidqa/translate.py
@@ -1,8 +1,11 @@
 class Translate(object):
     """translate solid csfasta file to fasta file"""
-    def __init__(self, csfasta, fasta):
+    def __init__(self, csfasta, fasta, qual = None, cutoff = 12, min_len = 25):
         self.csfasta = csfasta
         self.fasta = fasta
+        self.qual = qual
+        self.cutoff = cutoff
+        self.min_len = min_len
         self.color_space = {
         'A': {
             '0': 'A',
@@ -31,24 +34,48 @@ class Translate(object):
         }
 
     def translate(self):
+        """
+        if ft is True, filte low quality base when translate
+        """
         fin = open(self.csfasta, 'r')
         fo = open(self.fasta, 'w')
+        if self.qual:
+            fqual = open(self.qual, 'r')
+
         for line in fin:
+            if self.qual:
+                qline = fqual.readline()
             if line.startswith("#"):
                 continue
             elif line.startswith(">"):
                 fo.write(line)
             else:
                 line = line.strip()
-                fo.write(self._translate_read(line))
+                if self.qual:
+                    qline = qline.strip()
+                    fo.write(self._translate_read(line, qual = qline, cutoff = self.cutoff, min_len = self.min_len))
+                else:
+                    fo.write(self._translate_read(line))
                 fo.write("\n")
         fin.close()
         fo.close()
 
-    def _translate_read(self, read):
+    def _translate_read(self, read, **filte):
         seq = [read[0]]
         for b in read[1:]:
             if b == ".":
                 break
             seq.append(self.color_space[seq[-1]][b])
+        end = 1
+        if filte:
+            qual = filte.get("qual", [])
+            cutoff = filte.get("cutoff", 12)
+            min_len = filte.get("min_len", 25)
+            for q in qual:
+                if q < cutoff:
+                    break
+                end ++ 1
+            seq = seq[:end]
+            if len(seq) < min_len:
+                seq = []
         return "".join(seq)
